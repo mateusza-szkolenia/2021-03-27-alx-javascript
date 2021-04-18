fca70c6d60c875fc1575cb60c41f88aeeb7307c5
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-18 09:55:32 +0200

    komenarze w JS

--- dzien04/nauka01.js
+++ dzien04/nauka01.js
@@ -1,2 +1,23 @@
-document.querySelector("#pierwszy").textContent = "Aktualizuję pierwszy akapit";
+/*
+skrypt JS podmieniający zawartość dwóch akapitów
+
+autor: Mateusz A. (ALX) m.adamowski@alx.pl
+data: 2021-04-18
+*/
+
+
+// akcja:
+document.querySelector("#pierwszy").textContent = "Aktualizuję pierwszy akapit"; // poprawić
 document.querySelector("#drugi").textContent = "Aktualizuję drugi akapit";
+
+
+// komentarze służące do wyłączania fragmentów kodu
+
+/*
+
+document.querySelector("#trzeci").textContent = "Aktualizuję trzeci akapit";
+
+*/
+
+
+// document.querySelector("#czwarty").textContent = "Aktualizuję czwarty akapit";
