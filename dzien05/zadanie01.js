4c451b98d33959121a371c90c4ec8d57d64e080b
Mateusz Adamowski <m.adamowski@alx.pl>
2021-05-08 11:06:09 +0200

    pętla - przejście po podanych liczbach i wygenerowanie wierszy

--- dzien05/zadanie01.js
+++ dzien05/zadanie01.js
@@ -1,6 +1,21 @@
 function przelicz_btn(){
 
     var liczby = document.querySelector('#tempC').value.split("\n")
+    var tabelka = document.querySelector('#tabelka-wynikowa')
+
+    for (var i=0; i<liczby.length; i++){
+        var wiersz = document.createElement("tr")
+        var komC = document.createElement("td")
+        var komF = document.createElement("td")
+        var komOpis = document.createElement("td")
+        komC.innerText = liczby[i]
+
+        wiersz.append(komC)
+        wiersz.append(komF)
+        wiersz.append(komOpis)
+
+        tabelka.append(wiersz)
+    }
 
     console.log(liczby)
 
