1d2ed53a8cef7629c7b8d9c14fea44eede3c400e
Mateusz Adamowski <m.adamowski@alx.pl>
2021-05-08 11:17:08 +0200

    rozwiązanie

--- dzien05/zadanie01.js
+++ dzien05/zadanie01.js
@@ -9,8 +9,12 @@ function przelicz_btn(){
         var komC = document.createElement("td")
         var komF = document.createElement("td")
         var komOpis = document.createElement("td")
-        komC.innerText = liczby[i]
-        komF.innerText = C_to_F(parseFloat(liczby[i])).toFixed(1)
+    
+        var temperatura = liczby[i]
+
+        komC.innerText = temperatura
+        komF.innerText = C_to_F(parseFloat(temperatura)).toFixed(1)
+        komOpis.innerText = odczucie_temperatury(parseFloat(temperatura))
 
         wiersz.append(komC)
         wiersz.append(komF)
@@ -25,4 +29,20 @@ function przelicz_btn(){
 
 function C_to_F(tC){
     return 1.8 * tC + 32;
+}
+
+function odczucie_temperatury(tC){
+    if (tC < 0){
+        return "mróz"
+    }
+    if (tC < 8){
+        return "zimno" 
+    }
+    if (tC < 16){
+        return "chłodno"
+    }
+    if (tC < 25){
+        return "ciepło"
+    }
+    return "gorąco"
 }
